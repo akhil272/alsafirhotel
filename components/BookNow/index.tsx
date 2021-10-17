35b284e6e8629089a4216ddb6d837cf00d3611d2
akhil272 <akhilms272@gmail.com>
2021-10-17 22:20:20 +0530

    updated book now with tailwind slider open

--- components/BookNow/index.tsx
+++ components/BookNow/index.tsx
@@ -1,15 +1,18 @@
-import Modal from "./Modal";
-import { useState } from "react";
-import RoomsCount from "./RoomsCount";
 import Calendar from "./Calendar";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import ImageSlider from "../ImageSlider";
+import Calender from "./Calendar";
+import RoomsCount from "./RoomsCount";
 import PeopleCounter from "./PeopleCounter";
 import RoomSelector from "./RoomSelector";
+const image1 = "/images/homepage/homeCoverImage01.jpg";
 
 const BookNow = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const openModal = () => {
-    setShowModal((prev) => !prev);
+    setOpen((prev) => !prev);
   };
 
   return (
@@ -21,7 +24,91 @@ const BookNow = () => {
             <img src="/images/icons/call-icon-01.svg" />
           </div>
         </div>
-        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <Transition.Root show={open} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 overflow-hidden"
+            onClose={setOpen}
+          >
+            <div className="absolute inset-0 overflow-hidden">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-in-out duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-500"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              </Transition.Child>
+              <div className="fixed inset-y-0 right-0 w-screen flex">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-y-full"
+                  enterTo="translate-y-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-y-0"
+                  leaveTo="translate-y-full"
+                >
+                  <div className="relative w-screen max-w-md">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-in-out duration-500"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in-out duration-500"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="absolute "></div>
+                    </Transition.Child>
+                    <div className="fixed top-0 h-screen w-screen flex items-end">
+                      <div className="absolute h-full w-full">
+                        <ImageSlider images={[image1]} autoplay={false} />
+                      </div>
+                      <div className="h-3/5 w-full bg-black flex flex-col opacity-95 space-y-6 p-4">
+                        <div className="flex p-2 mt-6">
+                          <h3 className="uppercase font-mark text-2xl">
+                            Book Your Room
+                          </h3>
+                          <div
+                            onClick={openModal}
+                            className="flex justify-center items-center ml-auto"
+                          >
+                            <svg
+                              width="14"
+                              height="14"
+                              viewBox="0 0 14 14"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M8.46 7L14 12.54V14H12.54L7 8.46L1.46 14H0V12.54L5.54 7L0 1.46V0H1.46L7 5.54L12.54 0H14V1.46L8.46 7Z"
+                                fill="white"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                        <RoomSelector />
+                        <Calender />
+                        <RoomsCount />
+                        <div className="flex gap-2 justify-center gap ">
+                          <PeopleCounter categories="Adults" />
+                          <PeopleCounter categories="Children" />
+                        </div>
+                      </div>
+                      <div className="duration-1000 transition-all hover:bg-black hover:p-4  hover:text-primary fixed bottom-0 text-white bg-primary min-w-full text-center text-2xl p-2">
+                        <button>BOOK NOW</button>
+                      </div>
+                    </div>
+                  </div>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
       </div>
       <div className="hidden xl:block">
         <div className="fixed right-0 max-w-lg h-3/6 duration-1000 transition-all bottom-1/4 scale-100 ">
