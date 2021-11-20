aef921b5e3817305c8238dbae74018f1237d01c7
akhil272 <akhilms272@gmail.com>
2021-11-20 11:06:14 +0530

    added all icons for staypage

--- components/StayRoom/Amenities.tsx
+++ components/StayRoom/Amenities.tsx
@@ -2,7 +2,29 @@ import {
   deskIcon,
   lockerIcon,
   minibarIcon,
+  sittingAreaIcon,
+  wakeupServiceIcon,
+  teleIcon,
+  acIcon,
+  ironIcon,
+  channelsIcon,
+  tvIcon,
+  kettleIcon,
+  closetIcon,
+  HairdryerIcon,
+  bathrobeIcon,
+  slippersIcon,
+  bidetIcon,
+  bathtubeIcon,
+  toiletriesIcon,
+  wifiIcon,
+  parkingIcon,
+  elevatorIcon,
+  wheelchairIcon,
+  interconnectingroomsIcon,
 } from "../../public/images/RoomIconsPack";
+import IconsPack from "../../public/images/IconsPack";
+
 const Amenities = () => {
   return (
     <div className="flex flex-col font-mark text-secondary w-full md:w-2/3 gap-y-4  mt-20 mb-10 md:px-10 px-4">
@@ -22,41 +44,40 @@ const Amenities = () => {
               <div className="md:w-14 md:h-14 w-8 h-8">{minibarIcon}</div>
               <p>Minibar</p>
             </div>
-
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{sittingAreaIcon}</div>
               <p>Sitting area</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{wakeupServiceIcon}</div>
               <p>Wake-up service</p>
             </div>
           </div>
           <div className="flex flex-col space-y-4">
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{teleIcon}</div>
               <p>Telephone</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{acIcon}</div>
               <p>Air conditioning</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{ironIcon}</div>
               <p>Ironing facilities</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{channelsIcon}</div>
               <p>Satellite Channels</p>
             </div>
           </div>
           <div className="flex flex-col mt-4 md:mt-0 space-y-4">
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{tvIcon}</div>
               <p>Flat-screen TV</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{kettleIcon}</div>
               <p>Electric kettle</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
@@ -64,7 +85,7 @@ const Amenities = () => {
               <p>Towels/Sheets</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{lockerIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{closetIcon}</div>
               <p>Wardrobe or closet</p>
             </div>
           </div>
@@ -75,38 +96,34 @@ const Amenities = () => {
         <div className="md:flex md:space-x-4 grid grid-cols-2">
           <div className="flex flex-col space-y-4">
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{HairdryerIcon}</div>
               <p>Hairdryer</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{bathrobeIcon}</div>
               <p>Bathrobe</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{toiletriesIcon}</div>
               <p>Free toiletries</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{slippersIcon}</div>
               <p>Slippers</p>
             </div>
           </div>
           <div className="flex flex-col space-y-4">
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{bidetIcon}</div>
               <p>Bidet</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{closetIcon}</div>
               <p>Towels</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{bathtubeIcon}</div>
               <p>Bathtub or shower</p>
-            </div>
-            <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
-              <p>Slippers</p>
             </div>
           </div>
         </div>
@@ -116,24 +133,26 @@ const Amenities = () => {
         <div className="md:flex md:space-x-4">
           <div className="flex flex-col space-y-4">
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{wifiIcon}</div>
               <p>Free Wifi</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{parkingIcon}</div>
               <p>Free Parking</p>
             </div>
 
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{elevatorIcon}</div>
               <p>Upper floors accessible by elevator</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">{wheelchairIcon}</div>
               <p>Entire unit wheelchair accessible</p>
             </div>
             <div className="flex md:text-lg text-sm capitalize items-center md:gap-4 gap-1">
-              <div className="md:w-14 md:h-14 w-8 h-8">{deskIcon}</div>
+              <div className="md:w-14 md:h-14 w-8 h-8">
+                {interconnectingroomsIcon}
+              </div>
               <p>Interconnecting room (s) available</p>
             </div>
           </div>
