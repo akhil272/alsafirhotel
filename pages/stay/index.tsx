6cfe7c52ca57d680d950b329eb67a8fa4b5bc006
akhil272 <akhil272@hotmail.com>
2025-09-08 12:18:50 +0530

    feat(staypage): updated images and change from mm to cm for bed size

--- pages/stay/index.tsx
+++ pages/stay/index.tsx
@@ -43,10 +43,10 @@ const Stay = () => {
         <title>Stay | Al Safir Experience</title>
         <meta
           name="description"
-          content="Sophistic and splendid style Al Safir Hotel offers a classic stay. With 126 stylishly appointed rooms and suites that cleverly combine charm with discreet modern amenities."
+          content="Sophistic and splendid style Al Safir Hotel offers a classic stay. With 184 stylishly appointed rooms and suites that cleverly combine charm with discreet modern amenities."
         />
       </Head>
-      <ImageSlider autoPlayTime={10000} images={[coverImage02, coverImage01]}>
+      <ImageSlider autoPlayTime={10000} images={[coverImage02]}>
         <div className="flex flex-col h-full mt-24  px-2 md:p-0">
           <div className="md:p-14 p-0 flex flex-col">
             <div className="flex p-6">
@@ -72,7 +72,7 @@ const Stay = () => {
           title={t.room1}
           roomSize="29m2"
           guests="max 2 guests"
-          bedSize="1500mm wide"
+          bedSize="180cm wide"
           images={[image4, image3]}
           desc={t.room1description}
         />
@@ -81,7 +81,7 @@ const Stay = () => {
           title="Deluxe Twin"
           roomSize="29m2"
           guests="max 3 guests"
-          bedSize="2400mm wide"
+          bedSize="120cm wide"
           images={[image1, image2]}
           desc=" Treat yourself to a 5-star standard of living experience at one of
             the city's most distinguished addresses, Al Safir in Manama. Enjoy
@@ -93,7 +93,7 @@ const Stay = () => {
           title="Executive Single"
           roomSize="30m2"
           guests="max 3 guests"
-          bedSize="1500mm wide"
+          bedSize="150cm wide"
           images={[
             executive_single_room_image01,
             executive_single_room_image02,
@@ -105,7 +105,7 @@ const Stay = () => {
           title="Executive Twin"
           roomSize="30m2"
           guests="max 3 guests"
-          bedSize="1300mm wide"
+          bedSize="130cm wide"
           images={[executive_double_image01, executive_double_image02]}
           desc="Spacious 30 square meters Apartment with Twin bed, ample closet space, and bathroom with daily replenish of amenities. The room is also equipped with a work desk and has amazing city view.
           Each of these Executive Twin Apartment vibrant decor and combines luxurious furnishings with innovative technology to radiate a contemporary elegance."
@@ -114,7 +114,7 @@ const Stay = () => {
           title="Two-Bedroom Suite"
           roomSize="90m2"
           guests="max 6 guests"
-          bedSize="1800mm wide"
+          bedSize="180cm wide"
           images={[two_bedroom_suite_image01, two_bedroom_suite_image02]}
           desc="Spacious 90 square meter Two Bedroom Apartment with fully equipped kitchen. 1st bedroom with King Size bed, ample closet space, working desk, Bathroom en suite with Shower. 2nd bedroom with twin beds, ample closet space, bathroom en suite with shower.
           Each of the Tower wing Two Bedroom Apartment features vibrant decor, combining Executive leather furnishings with innovative technology to radiate a contemporary elegance and a separate Dining area."
@@ -123,7 +123,7 @@ const Stay = () => {
           title="Imperial Suite"
           roomSize="90m2"
           guests="max 6 guests"
-          bedSize="1800mm wide"
+          bedSize="180cm wide"
           images={[imperial_image01, imperial_image02]}
           desc="Spacious 175 square meter 2 Bedroom Imperial suite. 1st bedroom with King-size bed, ample closet space, working desk, Bathroom en suite with Shower. 2nd bedroom with two twin beds, ample closet space, bathroom en suite with shower. A separate spacious changing room with additional closet space.
           The luxurious spacious open kitchen has all the modern technology to create an imperial dinner.
